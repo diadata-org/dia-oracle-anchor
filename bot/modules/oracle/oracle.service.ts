41a733b02d5651aca29bc69e361e310ae0acf240
nnn-gif <nicky.gurbani@gmail.com>
2023-10-05 21:34:47 +0530

    Update oracle.service.ts

--- bot/modules/oracle/oracle.service.ts
+++ bot/modules/oracle/oracle.service.ts
@@ -368,17 +368,20 @@ export default class OracleService {
         await this.submitRandomRound(rounds)
         return
       }
-      // for existing contracts
       const currentRound = Number(latestRandomnessRound.round)
 
       let rounds = Array.from({ length: currentRound - lastRoundFromContract + 1 }, (_, index) => lastRoundFromContract + index)
+      this._logger.info(`lastRoundFromContract ${lastRoundFromContract}`)
+      this._logger.info(`currentRound ${currentRound}`)
+
+      this._logger.info(`diff ${currentRound - lastRoundFromContract}`)
 
       if (max_historical !== 0 && !this._initalRun) {
         this._initalRun = true
         if (rounds.length > max_historical) {
           this._logger.info(`Maximum historical rounds to update ${max_historical}`)
-          rounds = rounds.splice(0, max_historical)
           rounds = rounds.sort((a, b) => b - a)
+          rounds = rounds.splice(0, max_historical)
         }
       }
       // divide rounds in buckets of 10
